3a1052c6bcdf4baa2d0a9850c3eefb4fd84ee85e
Simonedambra <simonedambra130102@gmail.com>
2024-07-05 17:05:13 +0200

    first commit

---
+++ resources/views/parts/header.blade.php
@@ -0,0 +1,30 @@
+
+
+
+    <!-- inizio header -->
+    <header>
+        <div class="row">
+            <div class="container-header">
+                <div class="header-box">
+                    <div class="col-30">
+                        <ol class="row">
+                            <li class="header-list">Donna</li>
+                            <li class="header-list">Uomo</li>
+                            <li class="header-list">Bambini</li>
+                        </ol>
+                    </div>
+                    <div class="col-40 row">
+                        <div class="logo"></div>
+                    </div>
+                    <div class="col-30">
+                        <ol class="row">
+                            <li>&hearts;</li>
+                            <li>&clubs;</li>
+                            <li>&diams;</li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- fine header -->
